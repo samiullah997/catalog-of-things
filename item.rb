e9b4054b24936ee0b4af67253c9e864ee0d84341
t-bello7 <bello4aus@gmail.com>
2022-11-14 17:59:16 +0100

    - create test for item class - create tests for methods in Item class - implement can_be_archived and move_to_archive methods for Item class

--- item.rb
+++ item.rb
@@ -1,23 +1,24 @@
+# frozen_string_literal: true
+
+# Class for creating Item
 class Item
+  attr_reader :archived
 
-    def initialize(genre, author, source, label, publish_date, archived)
-        @id = Random.rand(1...1000)
-        @genre=genre
-        @author=author
-        @source=source
-        @label=label
-        @publish_date=publish_date
-        @archived=archived
-    end
+  def initialize(genre, author, source, label, publish_date)
+    @id = Random.rand(1...1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = false
+  end
 
-    def can_be_archived
-        # return true if publshed_date is older than 10yeard
-    end
+  def can_be_archived
+    ((Time.now - @publish_date) / 31_557_600).round(2) > 10
+  end
 
-    def move_to_archive
-#         should reuse can_be_archived?() method.
-# should change the archived property to true if the result of the can_be_archived?() method is true.
-# should do nothing if the result of the can_be_archived?() method is false.
-    end
-
-def
+  def move_to_archive
+    @archived = true if can_be_archived == true
+  end
+end

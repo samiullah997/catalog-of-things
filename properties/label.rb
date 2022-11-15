4de47ec5a7d8dbe7660c6b452e3008044cd5bc5d
samiullah997 <samiullahk997@gmail.com>
2022-11-15 10:12:46 +0500

    solve linter errors

--- properties/label.rb
+++ properties/label.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
+# create label class
 class Label
-    attr_accessor :title, :color
-    attr_reader :id, :items
+  attr_accessor :title, :color
+  attr_reader :id, :items
 
-    def initialize(title, color, id)
-        @id = Random.rand(1..100)
-        @title = title
-        @color = color
-        @items = []
-    end
+  def initialize(title, color, _id)
+    @id = Random.rand(1..100)
+    @title = title
+    @color = color
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        item.add_label(self)
-    end
+  def add_item(item)
+    @items << item
+    item.add_label(self)
+  end
 end
--- things/book.rb
+++ things/book.rb
@@ -1,17 +1,20 @@
+# frozen_string_literal: true
+
 require_relative '../item'
 
+# create book class inherite from item class
 class Book < Item
-    attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state
 
-    def initialize(publisher, cover_state, *args)
-        super(*args)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+  def initialize(publisher, cover_state, *args)
+    super(*args)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    private
+  private
 
-    def can_be_archived?
-        super || @cover_state == 'bad'
-    end
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
 end
